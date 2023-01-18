443651a5a96543150a3afb25072fb1b1fb143fab
Alim <56486795+Alimbo-zzz@users.noreply.github.com>
2023-01-19 01:21:59 +0300

    app init

---
+++ src/components/FSA/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import scss from './styles.module.scss'
+
+const FSA = (events) => {
+	const {} = events;
+	
+	return (<>
+		<div className={`${scss.wrap} container`}>
+
+			<p>FSA</p>
+
+		</div>
+	</>);
+}
+
+export default FSA;
